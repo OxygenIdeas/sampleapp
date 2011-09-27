ba90398f4af0a4452543abad0864d216e6df95a7
Manu <@>
2011-09-28 00:40:42 +0200

    Tests modificados con before(:each)

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -11,8 +15,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title",
-                                    :content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector("title", :content => @base_title + " | Home")
     end
   end
 
@@ -23,9 +26,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title",
-                                    :content =>
-                                        "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title", :content => @base_title + " | Contact")
     end
   end
 
@@ -36,8 +37,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title",
-                                    :content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title", :content => @base_title + " | About")
     end
   end
 end
